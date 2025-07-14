f0bd8ca2284baef43a12a788b9133f0da411c2d5
Menelik <menelikdev@gmail.com>
2025-07-14 12:54:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 // import required modules and components
-const API_BASE_URL = 'http://localhost:3000'; // Asegúrate de que coincida con el puerto de tu json-server
+const API_BASE_URL = 'http://localhost:3000';
 import { router } from './app/router.js';
 
 // When the DOM is fully loaded, initialize the router
